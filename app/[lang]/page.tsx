92600b823458638451761fcec16561b7dbf8d312
hexoul <crosien@gmail.com>
2025-06-06 23:37:03 +0900

    perf: optimize file size

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
 import { getDictionary } from "../../get-dictionary";
